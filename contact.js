2e391f4716982a71d0326b130494bd877b6cffc5
unknown <saishubhakar97@gmail.com>
2024-01-28 00:31:51 +0530

    changes Done

---
+++ contact.js
@@ -0,0 +1,37 @@
+let Name = document.getElementById("nameinput");
+let Email = document.getElementById("email");
+let textarea = document.getElementById("query");
+let button = document.getElementById("submitbutton");
+let nameSmall = document.getElementById("nameSmall");
+let emailSmall = document.getElementById("emailSmall");
+let query = document.getElementById("query");
+
+button.addEventListener("click",(e)=>{
+    let text = Name.innerText
+    console.log(text)
+})
+
+
+
+
+let validateInput = (input, regex, smallElement)=> {
+    input.addEventListener("blur", (e) => {
+        let text = e.target.value;
+
+        if (regex.test(text)) {
+            console.log("Success");
+            smallElement.style.display = "none";
+        } else {
+            smallElement.style.display = "block";
+        }
+    });
+}
+
+let nameRegex = /^[a-zA-Z-' ]+$/;
+let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+
+
+validateInput(Name, nameRegex, nameSmall)
+validateInput(Email, emailRegex, emailSmall)
